f19f4682543c83d4be507ad1efe90dab3d82bf52
vantu <tnn209201@gmail.com>
2023-09-24 23:14:38 +0700

    c5

--- lib/JSON/main.dart
+++ lib/JSON/main.dart
@@ -1,65 +1,36 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'user_model.dart';
-
-
-const jsonPath = '../asset/json/user_model.json';
-
-void main(List<String> args) {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(
-        title: 'JSON Demo',
-      ),
-    ),
-  );
+import 'package:flutter/services.dart' show rootBundle;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String fileContent = await rootBundle.loadString('lib/asset/json/user.json');
+   Map<String, dynamic> data = jsonDecode(fileContent);
+  List<dynamic> jsonData = data['users'];
+  runApp(MyApp(jsonData));
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
+class MyApp extends StatelessWidget {
+  final List< dynamic> jsonData;
 
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<User>? users;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadJsonData();
-  }
-
-  Future<void> _loadJsonData() async {
-    final json = await rootBundle.loadString(jsonPath);
-    setState(() {
-      final result = jsonDecode(json);
-      final data = Result.fromJson(result);
-      users = data.results;
-    });
-    debugPrint('Data loaded from file');
-  }
+  MyApp(this.jsonData);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: users != null
-          ? ListView.builder(
-              key: UniqueKey(),
-              itemCount: users!.length,
-              itemBuilder: (context, index) {
-                User user = users![index];
-                return Text(user.toString());
-              },
-            )
-          : const Center(child: CircularProgressIndicator()),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('User Data'),
+        ),
+        body: ListView.builder(
+          itemCount: jsonData.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(jsonData[index]['id'].toString()),
+              subtitle: Text(jsonData[index]['name']),
+            );
+          },
+        ),
+      ),
     );
   }
 }
